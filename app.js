453858a8759598f0eeb18f5e303dc10e23aac516
dulina2004 <dulinagunasinghe.com>
2024-06-26 15:35:40 +0530

    add count

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 console.log("hi");
-
+let count = 0;
 let randomNumber = Math.floor(Math.random() * 10 + 1);
 console.log(randomNumber);
 
 function t() {
     let message;
     let guess = document.getElementById("input").value;
+    if (count >= 3) {
+        document.getElementById("display").innerHTML +=
+            "<h3>Your chances are over</h3>";
+        return;
+    }
+    count++;
     console.log(randomNumber > guess);
     if (guess > randomNumber) {
         console.log("guess>>");
